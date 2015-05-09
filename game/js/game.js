1af33531550caf06e6cf492115aa8221bbc81e96
Kevin Wilson <kevinwilso541@gmail.com>
2015-05-09 14:20:42 -0700

    made more changes to the game.js file for the door, now going to branch

--- game/js/game.js
+++ game/js/game.js
@@ -9,13 +9,13 @@ function preload() {
     //game.load.image('ground', 'assets/platform.png');
     //game.load.image('star', 'assets/star.png');
     game.load.spritesheet('dude', 'assets/Block Ninja/Spritesheet.png', 16, 16);
-    game.load.spritesheet('portal', 'assets/portal_sprite.png')
-
+    game.load.image('portal', 'assets/door-5.png');
 }
 
 var player;
 var layer;
 var cursors;
+var portal;
 
 var facing = 'left';
 
@@ -29,11 +29,16 @@ function create() {
     layer = map.createLayer("Tile Layer 1");
     layer.resizeWorld();
     player = game.add.sprite(48, 16, 'dude');
+    portal = game.add.sprite(1288,64,'portal');
     game.physics.enable(player, Phaser.Physics.ARCADE);
+    game.physics.enable(portal, Phaser.Physics.ARCADE);
 
     player.body.gravity.y = 0;
     player.body.collideWorldBounds = true;
     player.body.setSize(16,16);
+
+    portal.body.gravity.y = 0;
+    portal.body.collideWorldBounds = true;
 
     player.animations.add('left', [0,1,2,3], 10, true);
     player.animations.add('turn', [4], 10, true);
@@ -49,7 +54,7 @@ function update() {
 
     //  Collide the player and the stars with the platforms
     game.physics.arcade.collide(player, layer);
-
+    game.physics.arcade.overlap(player, portal, finish, null, this);
 
     //  Reset the players velocity (movement)
     player.body.velocity.x = 0;
@@ -102,4 +107,8 @@ function update() {
         else player.frame = 0;
     }
     
+}
+
+function finish(player, door) {
+    player.reset(48,16);
 }
