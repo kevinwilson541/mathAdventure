506d26ab62564b6a7a7f6a7e71a143723c4b50ef
Jason <jmkranz@gmail.com>
2015-05-17 12:02:53 -0700

    treasure chest functionality

--- game/js/game.js
+++ game/js/game.js
@@ -85,11 +85,11 @@ Ninja.Game.prototype = {
             chest.body.gravity.y = 0;
         }
 
-        pause_label = this.add.text(this.width-110, 5, 'Pause', { font: '24px Arial', fill: '#fff' });
+        pause_label = this.add.text(800-110, 5, 'Pause', { font: '24px Arial', fill: '#fff' });
         pause_label.inputEnabled = true;
         pause_label.fixedToCamera = true;
         
-        unpause_label = this.add.text(this.width-110, 5, 'Resume', { font: '24px Arial', fill: '#fff'});
+        unpause_label = this.add.text(800-110, 5, 'Resume', { font: '24px Arial', fill: '#fff'});
         unpause_label.inputEnabled = true;
         unpause_label.fixedToCamera = true;
         unpause_label.visible = false;
@@ -111,6 +111,7 @@ Ninja.Game.prototype = {
                 unpause_label.visible = false;
             }
         }); 
+    
     },
     update: function () {
         //  Collide the player and the stars with the platforms
@@ -174,5 +175,17 @@ Ninja.Game.prototype = {
     },
     collect: function (player, chest) {
         chest.kill();
-    }
+	this.game.paused = true;  	
+	overlay();
+         
+	var button = document.getElementById('chestButton');
+	
+	var self = this;	
+	button.addEventListener("click", function (){
+		var answer = document.getElementById('chestAnswer').value;
+		alert(answer);
+		overlay();
+		self.game.paused = false;
+	});
+    },  
 }
